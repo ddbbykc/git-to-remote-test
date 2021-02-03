fa68e1db7f522a953c28765ef80dc067b38f2c92
Kathy Cuevas <ddbbykc@gmail.com>
2021-02-02 21:08:45 -0500

    new changes should reflect in Netlify

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Just putting something new here, haa ha! It worked this time, but I just added a plugin and am going to try it again...we'll see what happens.
+          Breaking Changes! Whoa!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
